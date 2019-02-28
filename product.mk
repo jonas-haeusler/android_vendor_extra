f61ca5ac3e21fbcab6ed80259c94dcf5c25b26d9
Jonas Häusler <haeusler@airmail.cc>
2019-02-28 16:27:32 +0100

    Include prebuilt apps for microG and FDroid

---
+++ product.mk
@@ -0,0 +1,12 @@
+# MicroG and FDroid packages
+PRODUCT_PACKAGES += \
+    com.google.android.maps.jar \
+    FakeStore \
+    FDroid \
+    FDroidPrivilegedExtension \
+    GmsCore \
+    GsfProxy \
+    MozillaNlpBackend \
+    NominatimNlpBackend \
+    OpenWeatherMapWeatherProvide           
+
